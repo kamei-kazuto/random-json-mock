388a296432c45b3ce27e11215cbba9a19fd83b80
kamei-kazuto <info.pscreator@gmail.com>
2018-12-18 11:30:14 +0900

    init

---
+++ webpack.dev.config.js
@@ -0,0 +1,22 @@
+const { join, resolve } = require('path')
+const Dotenv = require('dotenv-webpack')
+
+const config = {
+  entry: resolve('src', 'app.tsx'),
+  devtool: 'inline-source-map',
+
+  output: {
+    filename: '[name].bundle.js',
+    path: resolve('build')
+  },
+
+  plugins: [
+    new Dotenv({
+      path: 'development.env',
+      safe: false
+    }),
+  ],
+
+};
+
+module.exports = config
