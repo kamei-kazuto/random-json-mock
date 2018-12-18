63a59002890cc7282c2858f26434ea7955ee64e3
kamei-kazuto <info.pscreator@gmail.com>
2018-12-18 12:21:33 +0900

    update query Response

--- src/random-response.ts
+++ src/random-response.ts
@@ -2,5 +2,22 @@ import * as Express from 'express';
 import * as faker from 'faker'
 
 export const randomResponse = (req: Express.Request, res: Express.Response) => {
-  return res.send(faker.name.findName());
+  const response = randomJson(req.query)
+  res.send(response)
+};
+
+const randomJson = (query: {[key: string]: string}): object => {
+  console.log(query)
+  return Object.keys(query).map( (key, index) => {
+   return {
+      [key]: fakerValue(query[key])
+    }
+  }).reduce((o1: object, o2: object) => ({...o1, ...o2 }))
+}
+
+const fakerValue = ( type: string ): any  => {
+  switch (type) {
+    default:
+      return faker.name.title()
+  }
 }
